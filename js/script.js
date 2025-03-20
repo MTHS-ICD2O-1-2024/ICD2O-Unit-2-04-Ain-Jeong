afa09fa39c902b51a3997cc77e8673b1b430c73f
Ain-Jeong <ain.jeong@stu.ocsb.ca>
2025-03-20 01:26:42 +0000

    init

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Ain Jeong All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Ain Jeong
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
 
+"use strict"
+
 /**
- * Displays an alert with the message "Hello, World!"
+ * This function calculates area of a triangle.
  */
-function myButtonClicked() {
-  alert("Hello, World!")
+function calculateAreaOfTriangle () {
+  // input
+  const lengthOfTriangle = parseInt(document.getElementById('length-of-triangle').value)
+  const heightOfTriangle = parseInt(document.getElementById('width-of-triangle').value)
+
+  // process
+  const areaOfTriangle = (lengthOfTriangle * heightOfTriangle) / 2
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + areaOfTriangle + ' cm²'
 }
